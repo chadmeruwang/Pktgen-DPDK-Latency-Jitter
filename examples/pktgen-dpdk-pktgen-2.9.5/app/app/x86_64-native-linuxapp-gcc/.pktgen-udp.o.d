1e5fcca7e0b91997b1c33e6b585d118aeb83bf4b
root <root@terabit-server-23.(none)>
2016-01-29 02:10:31 -0500

    Revert "rm examples dir"
    This reverts commit 340b35052da82c187c54245cb1a394ee7dd6fc37.

---
+++ examples/pktgen-dpdk-pktgen-2.9.5/app/app/x86_64-native-linuxapp-gcc/.pktgen-udp.o.d
@@ -0,0 +1,186 @@
+dep_pktgen-udp.o = \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-udp.c \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen.h \
+ /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/inttypes.h \
+ /usr/include/unistd.h /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd.h /usr/include/string.h \
+ /usr/include/xlocale.h /usr/include/x86_64-linux-gnu/bits/string.h \
+ /usr/include/x86_64-linux-gnu/bits/string2.h \
+ /usr/include/x86_64-linux-gnu/bits/string3.h \
+ /usr/include/x86_64-linux-gnu/sys/queue.h /usr/include/netinet/in.h \
+ /usr/include/x86_64-linux-gnu/sys/socket.h \
+ /usr/include/x86_64-linux-gnu/sys/uio.h \
+ /usr/include/x86_64-linux-gnu/bits/uio.h \
+ /usr/include/x86_64-linux-gnu/bits/socket.h \
+ /usr/include/x86_64-linux-gnu/bits/sockaddr.h \
+ /usr/include/x86_64-linux-gnu/asm/socket.h \
+ /usr/include/asm-generic/socket.h \
+ /usr/include/x86_64-linux-gnu/asm/sockios.h \
+ /usr/include/asm-generic/sockios.h \
+ /usr/include/x86_64-linux-gnu/bits/socket2.h \
+ /usr/include/x86_64-linux-gnu/bits/in.h /usr/include/net/if.h \
+ /usr/include/fcntl.h /usr/include/x86_64-linux-gnu/bits/fcntl.h \
+ /usr/include/x86_64-linux-gnu/bits/stat.h \
+ /usr/include/x86_64-linux-gnu/bits/fcntl2.h /usr/include/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp2.h /usr/include/ctype.h \
+ /usr/include/errno.h /usr/include/x86_64-linux-gnu/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/termios.h /usr/include/x86_64-linux-gnu/bits/termios.h \
+ /usr/include/x86_64-linux-gnu/sys/ttydefaults.h \
+ /usr/include/x86_64-linux-gnu/sys/ioctl.h \
+ /usr/include/x86_64-linux-gnu/bits/ioctls.h \
+ /usr/include/x86_64-linux-gnu/asm/ioctls.h \
+ /usr/include/asm-generic/ioctls.h /usr/include/linux/ioctl.h \
+ /usr/include/x86_64-linux-gnu/asm/ioctl.h \
+ /usr/include/asm-generic/ioctl.h \
+ /usr/include/x86_64-linux-gnu/bits/ioctl-types.h /usr/include/libgen.h \
+ /usr/include/linux/if_tun.h /usr/include/linux/types.h \
+ /usr/include/x86_64-linux-gnu/asm/types.h \
+ /usr/include/asm-generic/types.h /usr/include/asm-generic/int-ll64.h \
+ /usr/include/x86_64-linux-gnu/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types_64.h \
+ /usr/include/linux/if_ether.h /usr/include/linux/filter.h \
+ /usr/include/arpa/inet.h /usr/include/netdb.h /usr/include/rpc/netdb.h \
+ /usr/include/x86_64-linux-gnu/bits/netdb.h /usr/include/poll.h \
+ /usr/include/x86_64-linux-gnu/sys/poll.h \
+ /usr/include/x86_64-linux-gnu/bits/poll.h /usr/include/assert.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_version.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_common.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/mm_malloc.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_errno.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_per_lcore.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_log.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_tailq.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_debug.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_memory.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/exec-env/rte_dom0_common.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_memcpy.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_vect.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/immintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/avxintrin.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_memzone.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_malloc.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_eal.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_launch.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_atomic.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/generic/rte_atomic.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_atomic_64.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_cycles.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/generic/rte_cycles.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_branch_prediction.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_prefetch.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/generic/rte_prefetch.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_lcore.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_pci.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_interrupts.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/exec-env/rte_interrupts.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_random.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_timer.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_ether.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_mbuf.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_mempool.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_ring.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_byteorder.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/generic/rte_byteorder.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_byteorder_64.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_ethdev.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_dev.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_devargs.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_ether.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_eth_ctrl.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_dev_info.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_scrn.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_copyright_info.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_l2p.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_port_config.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_core_info.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_rdline.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_cirbuf.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_vt100.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_parse.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_socket.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_parse_string.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_parse_num.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_parse_ipaddr.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_parse_etheraddr.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline_parse_portlist.h \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/cmdline.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_pcap.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_inet.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/nmmintrin.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_cksum.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_cycles.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_mbuf.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_coremap.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_lscpu.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/../lib/common/wr_utils.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-port-cfg.h \
+ /usr/include/pcap/pcap.h /usr/include/x86_64-linux-gnu/sys/time.h \
+ /usr/include/pcap/bpf.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-seq.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-constants.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-range.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-stats.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-pcap.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-dump.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-ether.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-random.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-capture.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-log.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/app/pktgen-udp.h
