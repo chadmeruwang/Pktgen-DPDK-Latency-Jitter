0a91df1a8771778158d375bb453e6d2b89c644dd
root <root@terabit-server-23.(none)>
2016-01-29 02:03:33 -0500

    edited source codes modified

---
+++ examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lib/lua-5.3.0/src/x86_64-native-linuxapp-gcc/.lfunc.o.d
@@ -0,0 +1,30 @@
+dep_lfunc.o = \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lfunc.c \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lprefix.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stddef.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lua.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdarg.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/luaconf.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lfunc.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lobject.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/llimits.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lgc.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lstate.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/ltm.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lzio.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lmem.h
---
+++ examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lib/lua-5.3.0/src/x86_64-native-linuxapp-gcc/.ltable.o.d
@@ -0,0 +1,48 @@
+dep_ltable.o = \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/ltable.c \
+ /root/dpdk-2.1.0/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lprefix.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/float.h /usr/include/math.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h /usr/include/string.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/string.h \
+ /usr/include/x86_64-linux-gnu/bits/string2.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/string3.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lua.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include/stdarg.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/luaconf.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/ldebug.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lstate.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lobject.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/llimits.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/ltm.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lzio.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lmem.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/ldo.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lgc.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lstring.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/ltable.h \
+ /root/dpdk-2.1.0/examples/pktgen-dpdk-pktgen-2.9.5/lib/lua-5.3.0/src/lvm.h
